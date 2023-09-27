cc846b4f9439eb1a8546aeea418b32694b4884f3
Vzsof10 <zsofivarga2003@gmail.com>
2023-09-27 15:40:49 +0200

    fixed return bug

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -79,11 +79,10 @@ public class GT4500 implements SpaceShip {
       case ALL:
         // try to fire both of the torpedo stores
         //TODO implement feature
-
         break;
     }
 
-    return firingSuccess;
+    return true;
   }
 
 }
